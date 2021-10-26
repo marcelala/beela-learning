517db7055065e82d53e2891f60da3f9af8703807
Marcela F <54597344+marcelala@users.noreply.github.com>
2021-10-26 22:04:33 +0200

    nav logo

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,6 +1,7 @@
 import { NavLink, useHistory } from "react-router-dom";
 import { useAuthentication } from "../context/AuthenticationContext";
 import { logOut } from "../firebaseServices/authentication";
+import logo from "assets/images/logo/beelaLogo.png";
 export default function Navigation() {
   const { isAuthenticated, setIsAuthenticated } = useAuthentication();
   const history = useHistory();
@@ -16,6 +17,11 @@ export default function Navigation() {
     <nav>
       <div className={"nav-content"}>
         <ul>
+          <li>
+            <NavLink to="/">
+              <img src={logo} alt="bee in a beehive" />
+            </NavLink>
+          </li>
           <li>
             <NavLink to="/home">Home</NavLink>
           </li>
